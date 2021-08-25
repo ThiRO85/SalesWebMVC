4edba16b7e9787be9f0cf09602f19d78a280b625
ThiRO85 <thgr85@gmail.com>
2021-08-25 15:33:10 -0300

    SellersController

---
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SellersController : Controller //Nome do controlador deve estar no plural, tal qual...
+    {                                           //... no arguivo _Layout.cshtml e na pasta Views
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
